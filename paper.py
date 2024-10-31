1a1a4fda2926231834db364db54b407d5a58c1ba
pejalgit71 <160094060+pejalgit71@users.noreply.github.com>
2024-10-31 23:20:57 +0800

    Update paper.py

--- paper.py
+++ paper.py
@@ -253,3 +253,5 @@ else:
         if st.button("Register"):
             register_user(new_username, new_name, new_password, new_role)
             st.success("User registered successfully!")
+st.sidebar.markdown("---")  # Adds a horizontal line for separation
+st.sidebar.markdown("Developed by Ts Faizal Ahmad Fadzil<sup>TM</sup>", unsafe_allow_html=True)
